6c26c6782435f33cbaf83eeedb02847bb722234f
Mykyta Krumalenko <prikol769@gmail.com>
2024-06-11 21:27:46 -0500

    added routers

--- api/app.js
+++ api/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import postRouter from "./routes/post.route.js";
 
 const app = express();
+
+app.use("/api/posts", postRouter);
 
 app.listen(8800, () => {
   console.log("server is running!");
