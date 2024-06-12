974a233f61673f97ff517db80de061c843535ac5
Mykyta Krumalenko <prikol769@gmail.com>
2024-06-11 21:46:16 -0500

    added hashedPassword

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,9 @@
-export const register = (req, res) => {
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
   const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 };
 
 export const login = (req, res) => {};
